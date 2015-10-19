8cc28faf68649b79a62d9f5ebf3e33a404a4fed7
unknown <hikingyo@INFO-DESIR.iut.bx1>
2015-10-19 10:59:37 +0200

    Init commit

---
+++ src/gameoftron/GameOfTron.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gameoftron;
+
+/**
+ *
+ * @author hikingyo
+ */
+    
+import java.io.PrintStream; 
+import java.util.Scanner;
+
+public class GameOfTron {
+    
+    static Scanner in = new Scanner (System.in);
+    static PrintStream out = System.out;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
